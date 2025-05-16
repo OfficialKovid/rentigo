85323853fca9f1228397cf04080bd283f6f9e15b
Kovid Tripathi <officialkovid@gmail.com>
2025-05-16 13:10:58 +0530

    chore: basic setup with index, templates, static, media

--- src/rentigo/settings.py
+++ src/rentigo/settings.py
@@ -40,9 +40,9 @@ INSTALLED_APPS = [
     
     # Custom apps
     'apps.accounts.apps.AccountsConfig',
-    'apps.homes.apps.HomesConfig',
+    'apps.home.apps.HomeConfig',
     'apps.rent.apps.RentConfig',
-    'apps.products.apps.ProductsConfig',
+    'apps.product.apps.ProductConfig',
 ]
 
 MIDDLEWARE = [
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'rentigo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],  # Add this line
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,6 +121,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- src/rentigo/urls.py
+++ src/rentigo/urls.py
@@ -17,11 +17,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.accounts.urls')),
-    path('', include('apps.homes.urls')),
+    path('', include('apps.home.urls')),
     path('', include('apps.rent.urls')),
-    path('', include('apps.products.urls')),
+    path('', include('apps.product.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
